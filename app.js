94bd44d07a83fcc0aab7497c5f67d899e072a9a5
OctavioOp <oc.opazoutlook.com>
2021-05-27 22:17:40 -0400

    [ADD] se agrego la obtencion de parametros de cada pokemon

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
     for(let i = 1; i<=151; i++){
-        $('#pokemon').append(`<div class="col-3 my-2"><img src=https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${i}.png alt=""></div>`)
+        $('#pokemon').append(`<div class="col-2 my-2"><img id="${i}" src=https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png alt=""></div>`)
     }
+
+    
+
+    $('img').on('click',function(){
+        let valueId = $(this).attr('id')
+        let name = '';
+        let photo = '';
+        let type = '';
+        let weigth = '';
+        let heiight = '';
+        //console.log(valueId);
+
+        $.get(`http://pokeapi.co/api/v2/pokemon/${valueId}`,function(data){
+            console.log(data);
+        name = data.name;
+        photo = data.sprites.front_default;
+        type = data.types;
+        weigth = data.weight
+        heiight = data.height
+        //console.log(heiight);
+        })
+    })
+
 })
